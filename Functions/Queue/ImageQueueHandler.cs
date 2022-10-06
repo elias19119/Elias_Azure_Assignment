a75436ed4f626536b7b6c35de3ce9065b9faa709
Elias Al Khouri Hanna <54850558+elias19119@users.noreply.github.com>
2022-10-06 14:41:49 +0200

    Add files via upload

---
+++ Functions/Queue/ImageQueueHandler.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Queues;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using ServerSideProgrammingAssignment.Service;
+
+namespace ServerSideProgrammingAssignment.Functions.Queue
+{
+    public class ImageQueueHandler
+    {
+        private static BlobContainerClient _uploadImageClient;
+        private static BlobContainerClient _editImageClient;
+        private static string _connection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+        private static string containerName = Environment.GetEnvironmentVariable("ContainerName");
+        [FunctionName("ImageQueueHandler")]
+        public async Task Run([QueueTrigger("image-queue", Connection = "AzureWebJobsStorage")]string uuid, ILogger log)
+        {
+            log.LogInformation($"Processing image of {uuid}");
+            _uploadImageClient = new(_connection, containerName);
+            _editImageClient = new(_connection, "edited-image-container");
+
+            BlobClient uploadclient = _uploadImageClient.GetBlobClient(uuid);
+            var response = await uploadclient.DownloadContentAsync();
+            var downloadedImage =  response.Value.Content.ToArray();
+            byte[] Image = downloadedImage;
+            byte[] modifiedImage;
+            try
+            {
+                modifiedImage =  ImageHelper.AddTextToImage(Image, ("processed ", (2, 2), 20, "0000FF", "FFC0CB"));
+            }
+            catch
+            {
+                return;
+            }
+            BlobClient editclient = _editImageClient.GetBlobClient(uuid);
+            await editclient.UploadAsync(new MemoryStream(modifiedImage));
+        }
+    }
+}
